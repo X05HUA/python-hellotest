02908e157cb9cf84517b7af62237b754d46a0979
X05HUA <61294910+X05HUA@users.noreply.github.com>
2020-10-28 22:33:01 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os.path
 from setuptools import setup
 
 VERSION_PATH = os.path.join(
-    os.path.dirname(__file__), 'helloworld/VERSION.txt')
+    os.path.dirname(__file__), 'hellotest/VERSION.txt')
 with io.open(VERSION_PATH, 'r', encoding='utf-8') as f:
   version = f.read().strip()
 
